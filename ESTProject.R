9b5a3b79239af395410afad8e0ca0821d223ea1f
Patrick Saunders <45730159+PatASaunders@users.noreply.github.com>
2020-04-16 21:16:47 +0200

    Add files via upload

--- ESTProject.R
+++ ESTProject.R
@@ -9,23 +9,38 @@ library(scales)
 library(vars)
 library(xts)
 library(readxl)
+library(tsibble)
+library(dplyr)
+library(padr)
 
-DJI_Data  <- read.csv("DJI_Data.csv", header=TRUE)
+DJI_Data  <- read_excel("DJI_Data.xlsx")
 Oil_Data  <- read_excel("Oil_Data.xls")
 Gold_Data <- read_excel("Gold_Data.xlsx")
 
-ggplot(DJI_Data, aes(x=Date, y=Close, group=1)) + geom_line() + ggtitle("Closing Price of DJ") + xlab("Date") + ylab("Price") + theme(plot.title = element_text(hjust = 0.5)) + scale_x_date(date_labels="%b %y", date_breaks = "5 years")
+DJI_Data$Date  <- as.Date(DJI_Data$Date, "%Y-%m-%d")
+Oil_Data$Date  <- as.Date(Oil_Data$Date, "%Y-%m-%d")
+Gold_Data$Date <- as.Date(Gold_Data$Date,"%Y-%m-%d")
 
-DJI_Time  <- as.POSIXct(paste(DJI_Data$Date, DJI_Data$Close), format = "%Y-%m-%d")
-Oil_Time  <- as.POSIXct(paste(Oil_Data$Date, Oil_Data$Close), format = "%Y-%m-%d")
-Gold_Time <- as.POSIXct(paste(Gold_Data$Date, Gold_Data$Close), format = "%Y-%m-%d")
+DJI_Data$Date %>% get_interval()
 
-DJI  <- xts(x=DJI_Data$Close, order.by = DJI_Time)
-Oil  <- xts(x=Oil_Data$Close, order.by = Oil_Time)
-Gold <- xts(x=Gold_Data$Close, order.by = Gold_Time)
+DJI_Data  <- DJI_Data  %>% pad()
+Oil_Data  <- Oil_Data  %>% pad()
+Gold_Data <- Gold_Data %>% pad()
+
+DJI_test <- DJI_Data %>% fill_by_function(fun = mean)
+
+#DJI_Data$Date  <- as.POSIXct(paste(DJI_Data$Date,  DJI_Data$Close),  format = "%d-%m-%d")
+#Oil_Data$Date  <- as.POSIXct(paste(Oil_Data$Date,  Oil_Data$Close),  format = "%Y-%m-%d")
+#Gold_Data$Date <- as.POSIXct(paste(Gold_Data$Date, Gold_Data$Close), format = "%Y-%m-%d")
+
+#DJI  <- xts(x=DJI_Data$Close,  order.by = DJI_Time)
+#Oil  <- xts(x=Oil_Data$Close,  order.by = Oil_Time)
+#Gold <- xts(x=Gold_Data$Close, order.by = Gold_Time)
+
+pad(DJI_Data)
 
 chartSeries(DJI)
 chartSeries(Oil)
 chartSeries(Gold)
 
-rDJI <- dailyReturn(DJI)
+tsibble()
